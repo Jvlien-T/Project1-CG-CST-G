2b0a772c027053dbebeee6fe73213e5459c0644f
Julien <Julien@LAPTOP-0ACNT8HS>
2018-01-28 20:56:24 +0100

    na

--- CodersStrikeBackGOLD/CSBMain.cs
+++ CodersStrikeBackGOLD/CSBMain.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 
 namespace CodersStrikeBackGOLD
 {
+    enum SectionLengthType { Close, Average, Far };
+    enum CurveStrengthType { Open, Medium, Hairpin };
     struct Coordinates
     {
         public int X { get; set; }
@@ -15,7 +17,41 @@ namespace CodersStrikeBackGOLD
 
     static class CSBCompute
     {
+        // fonction qui nous donne la distance entre deux points A et B :
+        static double DistAB(Coordinates A, Coordinates B) { return Math.Sqrt(Math.Pow(Math.Abs(A.X - B.X), 2) + Math.Pow(Math.Abs(A.Y - B.Y), 2)); }
 
+        // fonction qui nous donne une idée de la distance qui sépare deux points A et B :
+        static SectionLengthType DistBetTwoPoints(Coordinates A, Coordinates B)
+        {
+            double Dist = DistAB(A, B);
+            if (Dist > 8000) { return SectionLengthType.Far; }
+            else if (Dist > 3000) { return SectionLengthType.Average; }
+            else { return SectionLengthType.Close; }
+        }
+
+        // fonction qui nous donne l'angle ACB
+        static double AngleACB(Coordinates A, Coordinates B, Coordinates C)
+        {
+            return Math.Acos((Math.Pow(DistAB(B, C), 2) + Math.Pow(DistAB(A, C), 2) - Math.Pow(DistAB(A, B), 2)) / (2 * DistAB(B, C) * DistAB(A, C)));
+        }
+
+        // fonction qui nous donne une idée de l'angle entre trois points ACB
+        static CurveStrengthType AngleBetThreePoints(Coordinates A, Coordinates B, Coordinates C)
+        {
+            double Angle = Math.Abs(AngleACB(A, B, C));
+            if (Angle > 120) { return CurveStrengthType.Open; }
+            else if (Angle > 60) { return CurveStrengthType.Medium; }
+            else { return CurveStrengthType.Hairpin; }
+        }
+
+        //fonction qui nous donne la distance a laquelle on devrait passer au plus proche du prochain WP
+        static double ClosestFromNxtWP(Coordinates PreviousPos, Coordinates CurrentPos, Coordinates CPPos, double NextCPDistance)
+        {
+            double DriftAngle = AngleACB(CurrentPos, CPPos, PreviousPos);
+            double AbsGap = NextCPDistance * Math.Tan(DriftAngle);
+            if (NextCPDistance < 900 && AbsGap > 600) { AbsGap = -1; }
+            return AbsGap;
+        }
     }
 
     class CSBPod
@@ -26,6 +62,17 @@ namespace CodersStrikeBackGOLD
         private int p_myangle;
         private int p_mynextCPID;
 
+        public CSBPod()
+        {
+            p_myprevpos.X = 0;
+            p_myprevpos.Y = 0;
+            p_mypos.X = 0;
+            p_mypos.Y = 0;
+            p_myspeed.X = 0;
+            p_myspeed.Y = 0;
+            p_myangle = 0;
+            p_mynextCPID = 0;
+        }
         public void Update(int myposX, int myposY, int myspeedX, int myspeedY, int myangle, int mynextCPID)
         {
             p_myprevpos.X = p_mypos.X == 0 ? myposX : p_mypos.X;
